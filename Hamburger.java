ff2087ef9917188ffa39639c0cac7b85691ded9f
nYcnela <86907753+nYcnela@users.noreply.github.com>
2022-09-02 19:40:21 +0200

    Update Hamburger.java

--- Hamburger.java
+++ Hamburger.java
@@ -29,31 +29,36 @@ public class Hamburger {
 
     public void addHamburgerAddition1 (String type, double pricee){
         if(!type.isEmpty()){
-            price += pricee;
-            System.out.println("Added " + type + " for " + pricee);
+            this.addition1Name = type;
+            this.addition1Price = pricee;
+            System.out.println("Added " + this.addition1Name + " for " + this.addition1Price);
         }
     }
     public void addHamburgerAddition2 (String type, double pricee){
         if(!type.isEmpty()){
-            price += pricee;
-            System.out.println("Added " + type + " for " + pricee);
+            this.addition2Name = type;
+            this.addition2Price = pricee;
+            System.out.println("Added " + this.addition2Name + " for " + this.addition2Price);
         }
     }
     public void addHamburgerAddition3 (String type, double pricee) {
         if(!type.isEmpty()){
-            price += pricee;
-            System.out.println("Added " + type + " for " + pricee);
+            this.addition3Name = type;
+            this.addition3Price = pricee;
+            System.out.println("Added " + this.addition3Name + " for " + this.addition3Price);
         }
     }
     public void addHamburgerAddition4 (String type, double pricee) {
         if(!type.isEmpty()){
-            price += pricee;
-            System.out.println("Added " + type + " for " + pricee);
+            this.addition4Name = type;
+            this.addition4Price = pricee;
+            System.out.println("Added " + this.addition4Name + " for " + this.addition4Price);
         }
     }
 
     public double itemizeHamburger(){
-        return price;
+        return addition1Price + addition2Price + addition3Price + addition4Price + price;
     }
 }
+
 
