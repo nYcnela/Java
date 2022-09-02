0a4e6e98430f6f0377c388838e63e88f266618fe
nYcnela <86907753+nYcnela@users.noreply.github.com>
2022-09-02 21:27:28 +0200

    Update DeluxeBurger.java

--- DeluxeBurger.java
+++ DeluxeBurger.java
@@ -1,42 +1,36 @@
 public class DeluxeBurger extends Hamburger{
 
-    private String name = "Deluxe";
-    private String meat = "Sausage & Bacon";
-    private double price = 14.54;
-    private String breadRollType = "White roll";
-
-
-
-
-
+    public DeluxeBurger(){
+        super("Deluxe", "Sausage & Bacon", 14.54, "White");
+        super.addHamburgerAddition1("Chips", 2.75);
+        super.addHamburgerAddition2("Drink", 1.81);
+    }
     @Override
-    public void addHamburgerAddition1(String type, double pricee) {
+    public void addHamburgerAddition1(String type, double price) {
         System.out.println("Cannot not add additional items to a deluxe burger");
     }
 
     @Override
-    public void addHamburgerAddition2(String type, double pricee) {
+    public void addHamburgerAddition2(String type, double price) {
         System.out.println("Cannot not add additional items to a deluxe burger");
     }
 
     @Override
-    public void addHamburgerAddition3(String type, double pricee) {
+    public void addHamburgerAddition3(String type, double price) {
         System.out.println("Cannot not add additional items to a deluxe burger");
     }
 
     @Override
-    public void addHamburgerAddition4(String type, double pricee) {
+    public void addHamburgerAddition4(String type, double price) {
         System.out.println("Cannot not add additional items to a deluxe burger");
     }
-
     @Override
     public double itemizeHamburger() {
-        System.out.println(name + " hamburger on a " + breadRollType+ " with " + meat + ", price is " + price);
-        System.out.println("Added Chips for an extra 2.75");
-        System.out.println("Added Drink for an extra 1.81");
-        return 19.10;
+        double deluxeBurgerPrice = super.itemizeHamburger();
+        return deluxeBurgerPrice;
     }
 }
+
 
 
 
