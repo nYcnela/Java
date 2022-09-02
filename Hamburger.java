e101a7ca3e112493b98c4e823d739044ce3ace63
nYcnela <86907753+nYcnela@users.noreply.github.com>
2022-09-02 21:27:09 +0200

    Update Hamburger.java

--- Hamburger.java
+++ Hamburger.java
@@ -14,50 +14,52 @@ public class Hamburger {
     private double addition4Price;
 
 
-    public Hamburger() {
-    }
-
     public Hamburger(String name, String meat, double price, String breadRollType) {
         this.name = name;
         this.meat = meat;
         this.breadRollType = breadRollType;
         this.price = price;
-        System.out.println(name + " hamburger on a " + breadRollType + " with " + meat + ", price is " + price);
     }
 
 
 
-    public void addHamburgerAddition1 (String type, double pricee){
-        if(!type.isEmpty()){
+    public void addHamburgerAddition1 (String type, double price){
             this.addition1Name = type;
-            this.addition1Price = pricee;
-            System.out.println("Added " + addition1Name + " for " + addition1Price);
-        }
+            this.addition1Price = price;
     }
-    public void addHamburgerAddition2 (String type, double pricee){
-        if(!type.isEmpty()){
+    public void addHamburgerAddition2 (String type, double price){
             this.addition2Name = type;
-            this.addition2Price = pricee;
-            System.out.println("Added " + addition2Name + " for " + addition2Price);
-        }
+            this.addition2Price = price;
     }
-    public void addHamburgerAddition3 (String type, double pricee) {
-        if(!type.isEmpty()){
+    public void addHamburgerAddition3 (String type, double price) {
             this.addition3Name = type;
-            this.addition3Price = pricee;
-            System.out.println("Added " + addition3Name + " for " + addition3Price);
-        }
+            this.addition3Price = price;
     }
-    public void addHamburgerAddition4 (String type, double pricee) {
-        if(!type.isEmpty()){
+    public void addHamburgerAddition4 (String type, double price) {
             this.addition4Name = type;
-            this.addition4Price = pricee;
-            System.out.println("Added " + addition4Name + " for " + addition4Price);
-        }
+            this.addition4Price = price;
     }
 
     public double itemizeHamburger(){
-        return addition1Price + addition2Price + addition3Price + addition4Price + price;
+        double hamburgerPrice = this.price;
+        System.out.println(this.name + " hamburger on a " + this.breadRollType + " with " + this.meat + ", price is " + this.price);
+        if(this.addition1Name != null){
+            hamburgerPrice += this.addition1Price;
+            System.out.println("Added " + this.addition1Name + " for " + this.addition1Price);
+        }
+        if(this.addition2Name != null){
+            hamburgerPrice += this.addition2Price;
+            System.out.println("Added " + this.addition2Name + " for " + this.addition2Price);
+        }
+        if(this.addition3Name != null){
+            hamburgerPrice += this.addition3Price;
+            System.out.println("Added " + this.addition3Name + " for " + this.addition3Price);
+        }
+        if(this.addition4Name != null){
+            hamburgerPrice += this.addition4Price;
+            System.out.println("Added " + this.addition4Name + " for " + this.addition4Price);
+        }
+        return hamburgerPrice;
     }
 }
 
